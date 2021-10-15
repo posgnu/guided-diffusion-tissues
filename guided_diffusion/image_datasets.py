5cf1c88f209344d54bf233e4daaf835d003c8668
edeyneka <edeyneka@ics.uci.edu>
2021-10-15 15:14:12 -0700

    added a lot of dataset specific functions

--- guided_diffusion/image_datasets.py
+++ guided_diffusion/image_datasets.py
@@ -1,5 +1,6 @@
 import math
 import random
+import os
 
 from PIL import Image
 import blobfile as bf
@@ -7,6 +8,133 @@ from mpi4py import MPI
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
 
+def load_superres_data(
+    *,
+    paths,
+    batch_size,
+    patch_size,
+    deterministic=False,
+    random_crop=True,
+    random_flip=True,
+    ):
+    if not paths:
+        raise ValueError("unspecified paths")
+    dataset = SuperresImageDataset(
+                patch_size,
+                paths,
+                shard=MPI.COMM_WORLD.Get_rank(),
+                num_shards=MPI.COMM_WORLD.Get_size(),
+                random_crop=random_crop,
+                random_flip=random_flip,
+                )
+    if deterministic:
+        loader = DataLoader(
+            dataset, batch_size=batch_size, shuffle=False, num_workers=1, drop_last=True
+        )
+    else:
+        loader = DataLoader(
+            dataset, batch_size=batch_size, shuffle=True, num_workers=1, drop_last=True
+        )
+    while True:
+        yield from loader
+
+class SuperresImageDataset(Dataset):
+    def __init__(
+            self, 
+            patch_size,
+            paths,
+            shard=0,
+            num_shards=1,
+            random_crop=True,
+            random_flip=True,
+            ):
+        super().__init__()
+        self.resolution = resolution
+        self.local_images = image_paths[shard:][::num_shards]
+        self.random_crop = random_crop
+        self.random_flip = random_flip
+
+        def __len__(self):
+            return len(self.local_images)
+
+        def __getitem__(self, idx):
+            high_res_path = self.local_images[idx]
+            low_res_path = high_res_path.replace('high_res', 'low_res')
+
+            with bf.BlobFile(high_res_path, "rb") as f:
+                high_res_pil_image = Image.open(f)
+            with bf.BlobFile(low_res_path, "rb") as f:
+                low_res_pil_image = Image.open(f)
+            
+            if high_res_pil_image.size != low_res_pil_image:
+                return self.__getitem__(self, idx+1)
+
+            high_res_pil_image.load()
+            low_res_pil_image.load()
+
+            high_res_pil_image = high_res_pil_image.convert("RGB")
+            low_res_pil_image = low_res_pil_image.convert("RGB")
+
+            if self.random_crop:
+                high_res_arr, low_res_arr = random_crop_arr_input_target(
+                        high_res_pil_image,
+                        low_res_pil_image,
+                        patch_size)
+                if is_white(high_res_arr):
+                    return self.__getitem__(self, idx)
+
+            if self.random_flip and random.random() < 0.5:
+                high_res_arr = high_res_arr[:, ::-1]
+                low_res_arr = low_res_arr[:, ::-1]
+
+            high_res_arr = high_res_arr.astype(np.float32) / 127.5 - 1
+            low_res_arr = low_res_arr.astype(np.float32) / 127.5 - 1
+
+            out_dict = {}
+            out_dict["low_res"] = np.transpose(low_res_arr, [2, 0, 1])
+            return np.transpose(high_res_arr, [2, 0, 1]), out_dict
+
+def is_white(arr):
+    number_of_white_pix = np.sum(arr = [255, 255, 255])
+    all_pix = arr,shape[0] * arr.shape[1]
+    white_fraction = number_of_white_pix / all_pix
+    if white_fraction >= 0.95 :
+        return True
+    else:
+        return False
+    
+def _file_name(file_path):
+    basename = os.path.basename(file_path)
+    file_name = os.path.splitext(basename)[0]
+    return file_name
+
+def _list_image_files_train_valid_test(data_dir, valid_samples, test_samples):
+    paths = []
+    for entry in sorted(bf.listdir(data_dir)):
+        if len(paths) < 2: % This is just for faster pipeline testing, comment during actual training
+            full_path = bf.join(data_dir, entry)
+            ext = entry.split(".")[-1]
+            if "." in entry and ext.lower() in ["jpg", "jpeg", "png", "gif", "tif"]:
+                paths.append(full_path)
+            elif bf.isdir(full_path):
+                paths.extend(_list_image_files_train_valid_test(full_path)) 
+        else:
+            break
+
+    test_paths = [path for path in paths if _file_name(path) in test_samples]
+    valid_paths = [path for path in paths if _file_name(path) in valid_samples]
+    train_paths = [path for path in paths if (path not in test_paths) and (path not in valid_paths)]
+    
+    return train_paths, valid_paths, test_paths
+
+def random_crop_arr_input_target(pil_image_target, pil_image_input, patch_size):
+
+    arr_target = np.array(pil_image_target)
+    arr_input = np.array(pil_image_input) 
+    crop_y = random.randrange(arr_target.shape[0] - patch_size + 1)
+    crop_x = random.randrange(arr_target.shape[1] - patch_size + 1)
+    return arr_target[crop_y : crop_y + patch_size, crop_x : crop_x + patch_size],
+            arr_input[crop_y : crop_y + patch_size, crop_x : crop_x + patch_size]
 
 def load_data(
     *,
